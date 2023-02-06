361e476130ba9016f981182a370dfdc2a54df006
Yuliya_K <live2do@yandex.ru>
2023-02-06 19:32:28 +0300

    add Задача 23. Кубы чисел

---
+++ 23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+if (N < 0)
+{
+    i = N;
+    N = -1; 
+}
+
+for (; i <= N; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
